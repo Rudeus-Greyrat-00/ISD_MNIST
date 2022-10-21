93f491a1b9a06fb56f9a9099fe1a204ca174912f
Samuele Capacci <samuele_capacci@outlook.it>
2022-10-21 11:15:30 +0200

    --

--- data_perturb/c_data_perturb_random.py
+++ data_perturb/c_data_perturb_random.py
@@ -1,4 +1,5 @@
 from c_data_perturb import CDataPerturb
+import numpy as np
 
 class CDataPerturbRandom(CDataPerturb):
 
@@ -33,28 +34,21 @@ class CDataPerturbRandom(CDataPerturb):
 
     @K.setter
     def K(self, value):
-        if K < 0:
+        if value < 0:
             raise ValueError("K should be positive")
         self._K = value
 
     def data_perturbation(self, x):
         v = np.zeros(shape=x.shape[0])
         xp = np.zeros(shape=x.shape[0])
-        if K > x.shape[0]:
+        if self.K > x.shape[0]:
             v = np.ones(shape=x.shape[0])
         else:
-            v[0:K] = 1
+            v[0:self.K] = 1
             np.random.shuffle(v)
 
         xp[v==1] = np.random.uniform(self._min_value, self.max_value, size=self._K)
-
-
-
-
-
-
-
-
+        xp[v==0] = x[v==0]
 
 
 
