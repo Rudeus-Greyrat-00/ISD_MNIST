44d38f1168a65cc213c46725eb8bd391e9aea935
marko <m.ledda45@studenti.unica.it>
2022-10-21 11:54:29 +0200

    commit

---
+++ data_perturb/c_data_perturb_gaussian.py
@@ -0,0 +1,48 @@
+from c_data_perturb_random import CDataPerturbRandom
+from c_data_perturb import CDataPerturb
+import numpy as np
+
+class CDataPerturbGaussian(CDataPerturb):
+
+    def __init__(self, min_value=0, max_value=1, sigma=100.0):
+        self._min_value = min_value
+        self._max_value = max_value
+        self._sigma = sigma
+
+    @property
+    def min_value(self):
+        return self._min_value
+
+    @property
+    def max_value(self):
+        return self._max_value
+
+    @property
+    def sigma(self):
+        return self._sigma
+
+    @min_value.setter
+    def min_value(self, value):
+        if value < 0 and value > 1:
+            raise ValueError("Min value parameter should be within 0 and 1")
+        self._min_value = value
+
+    @max_value.setter
+    def max_value(self, value):
+        if value < 0 and value > 1 and value >= self._min_value:
+            raise ValueError("Max value parameter should be within 0 and 1 and greater than min value")
+        self._max_value = value
+
+    @sigma.setter
+    def sigma(self, value):
+        self._sigma = value
+
+    def data_perturbation(self, x):
+        """
+        Gaussian perturbation
+        :param x:
+        :return:
+        """
+        pass
+
+
